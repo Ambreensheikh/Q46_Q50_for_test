f07261c6d21da9a71372b91dd8bbba92314826a3
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-06-21 23:04:15 +0500

    first commit

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
 "use strict";
-//Q_46:create an object
-let laptop = {
-    make: "hp",
-    model: "pro book",
-    year: 2023,
-    describe: function () {
-        console.log(`My laptop is ${this.make} ,${this.model} ,${this.year}`);
-    }
+//Q_61 enums
+var TypeVehicle;
+(function (TypeVehicle) {
+    TypeVehicle[TypeVehicle["car"] = 0] = "car";
+    TypeVehicle[TypeVehicle["truck"] = 1] = "truck";
+    TypeVehicle[TypeVehicle["motorcycle"] = 2] = "motorcycle";
+})(TypeVehicle || (TypeVehicle = {}));
+console.log(TypeVehicle.truck);
+let student = {
+    rollNo: 1234,
+    id: "ali@com",
+    choices: ["IT", "AI", "cloud engineering"],
 };
-laptop.describe();
-//Q_47  Array destructuring
-let laptops = [
-    { make: "Dell", model: "XPS 15", year: 2021 },
-    { make: "Apple", model: "MacBook Pro", year: 2020 },
-    { make: "HP", model: "Spectre x360", year: 2021 },
-];
-let [laptop1, laptop2] = laptops;
-console.log(laptop1);
-console.log(laptop2);
-//Q_48 combining two arrays
-let priceList_a = [1200, 4500, 7500];
-let priceList_b = [600, 2300, 4000];
-let combinedPrices = [...priceList_a, ...priceList_b].sort((a, b) => a - b); //ascending order
-console.log(combinedPrices);
-//Q_49 function with rest parameters...
-function myHobbies(...hobbies) {
-    hobbies.forEach((hobby) => {
-        console.log(`I enjoy ${hobby}`);
-    });
+console.log(student);
+let circle = {
+    kind: "circle",
+    radius: 10,
+};
+let rectangle = {
+    kind: "rectangle",
+    width: 20,
+    length: 8,
+};
+console.log(circle);
+console.log(rectangle);
+//Q_64 making function string and number
+function MixTextNumber(text, number) {
+    return text + number;
 }
-;
-myHobbies("coding", "reading", "playing");
-//Q_50  multiline string literal
-let myIdealDay = `my ideal day spend when :
-  1. reciting Holy Quran early in the morning,
-  2. having favorite breakfast,
-  3. coding without any errors`;
-console.log(myIdealDay);
+console.log("My age is :", 30);
+//Q_65 use of modulus% as an arithmatic operator for remainder
+function Remainder(num1, num2) {
+    return num1 % num2;
+}
+console.log(Remainder(5, 2));
--- app.ts
+++ app.ts
@@ -1,40 +1,49 @@
-//Q_46:create an object
-let laptop={
-    make:"hp",
-    model:"pro book",
-    year:2023,
-    describe: function(){
-        console.log(`My laptop is ${this.make} ,${this.model} ,${this.year}`)
-    }
-};
-laptop.describe();
-//Q_47  Array destructuring
-let laptops = [
-    { make: "Dell", model: "XPS 15", year: 2021 },
-    { make: "Apple", model: "MacBook Pro", year: 2020 },
-    { make: "HP", model: "Spectre x360", year: 2021 },
-  ];
-  let [laptop1,laptop2] = laptops;
-  console.log(laptop1);
-  console.log(laptop2);
+//Q_61 enums
+enum TypeVehicle{
+    car,
+    truck,
+    motorcycle,
+}
+console.log(TypeVehicle.truck);
 
-  //Q_48 combining two arrays
-  let priceList_a = [1200, 4500, 7500];
-  let priceList_b = [600, 2300,4000];
-  let combinedPrices = [...priceList_a,...priceList_b].sort((a,b) =>a-b);//ascending order
-  console.log(combinedPrices);
+//Q-62 interface
+interface Student{
+    rollNo:number;
+    id : string;
+    choices:string[]
+}
+let student:Student={
+    rollNo:1234,
+    id:"ali@com",
+    choices:["IT", "AI", "cloud engineering"],
+}
+ console.log(student);
 
-  //Q_49 function with rest parameters...
-  function myHobbies(...hobbies:string[]){
-    hobbies.forEach((hobby) =>{
-  console.log(`I enjoy ${hobby}`)
-    })
-     };
-     myHobbies("coding", "reading", "playing");
-  
-  //Q_50  multiline string literal
-  let myIdealDay = `my ideal day spend when :
-  1. reciting Holy Quran early in the morning,
-  2. having favorite breakfast,
-  3. coding without any errors`
-  console.log(myIdealDay);
+ //Q_63 type alias
+ type Shape = {
+    kind: "circle" | "rectangle";
+    length? :number;
+    width? :number;
+    radius? : number;
+ }
+ let circle:Shape={
+    kind: "circle",
+    radius: 10,
+ };
+ let rectangle:Shape={
+    kind : "rectangle",
+    width : 20,
+    length: 8,
+ };
+ console.log(circle);
+ console.log(rectangle);
+ //Q_64 making function string and number
+ function MixTextNumber(text:string, number:number){
+    return text + number
+ }
+ console.log("My age is :" , 30);
+ //Q_65 use of modulus% as an arithmatic operator for remainder
+ function Remainder(num1:number,num2:number):number{
+    return num1 % num2
+ }
+ console.log(Remainder(5, 2));
