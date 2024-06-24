f58c9a0d9f9d7bd621685504b258c044e04684b1
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-06-25 00:24:06 +0500

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 "use strict";
-//Q_73 my variable assign
-let welcome = (name, id) => {
-    console.log(`Welcome ! your name is ${name} and your id is ${id}`);
-};
-welcome("ali", 24);
-welcome("hashir", 123);
-//Q_74 destructuring of two variables
-let a = 5;
-let b = 24;
-[a, b] = [b, a];
-console.log(`The value of a is ${a} and the value of b is ${b}`);
-//Q_75 assignment operators
-let x = 5;
-x += 2;
-console.log(`By addition :`, x);
-x - +4;
-console.log(`By substraction :`, x);
-x *= 6;
-console.log(`By multiply :`, x);
-x /= 2;
-console.log(`By division :`, x);
+// 
+//Q_86 create a function to find text true/false
+function detectJS(text) {
+    return text.includes("Javascript");
+}
+console.log(detectJS(`I love to learn Javascript`));
+//Q_87 make a function takes first ten characters from text
+function tenCharacters(text) {
+    return text.substring(0, 10);
+}
+console.log(tenCharacters(`Hello Javascript !`));
+//Q_88 make a function round a decimal number to a whole number
+function roundNumber(num) {
+    return Math.round(num);
+}
+console.log(roundNumber(34.87));
+//Q_89 function change string into number
+function changeString(str) {
+    return parseFloat(str);
+}
+console.log(changeString("374.90"));
+//Q_90 function check if value is not a number
+function check(valueType) {
+    return isNaN(valueType);
+}
+console.log(check("Hello!"));
+console.log(check("35"));
--- app.ts
+++ app.ts
@@ -1,27 +1,31 @@
-
-
-//Q_73 my variable assign
-let welcome=(name:string, id:number)=>{
-  console.log(`Welcome ! your name is ${name} and your id is ${id}`);
+// 
+//Q_86 create a function to find text true/false
+function detectJS(text :string):boolean{
+  return text.includes("Javascript");
 }
-welcome("ali" , 24);
-welcome("hashir" , 123);
+console.log(detectJS(`I love to learn Javascript`));
 
-//Q_74 destructuring of two variables
-let a = 5;
-let b = 24;
-[a , b] = [b , a]
-console.log(`The value of a is ${a} and the value of b is ${b}`);
+//Q_87 make a function takes first ten characters from text
+function tenCharacters(text :string):string{
+  return text.substring(0 , 10);
+}
+console.log(tenCharacters(`Hello Javascript !`));
 
-//Q_75 assignment operators
-let x = 5;
+//Q_88 make a function round a decimal number to a whole number
+function roundNumber(num:number):number{
+  return Math.round(num);
+}
+console.log(roundNumber(34.87));
 
-x += 2;
-console.log(`By addition :` , x);
-x -+ 4;
-console.log(`By substraction :` , x);
-x *= 6;
-console.log(`By multiply :`, x);
-x /= 2;
-console.log(`By division :`, x);
+//Q_89 function change string into number
+function changeString(str:string):number{
+  return parseFloat(str);
+}
+console.log(changeString("374.90")); 
 
+//Q_90 function check if value is not a number
+function check(valueType:any):boolean{
+  return isNaN(valueType);
+}
+console.log(check("Hello!"));
+console.log(check("35"));
